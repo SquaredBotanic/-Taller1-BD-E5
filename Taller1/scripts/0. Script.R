c57e72f70a957b7a426c4e1fccaf73982b521c64
Bontiac <alejandra.davilar@konradlorenz.edu.co>
2026-02-21 01:42:51 -0500

    general_structure_execution

--- Taller1/scripts/0. Script.R
+++ Taller1/scripts/0. Script.R
@@ -32,3 +32,36 @@ p_load(rio,              # Importacion y exportacion sencilla de datos
        dplyr)            # For post regression calculations      
 
 getwd() #Mirar directorio
+
+# 1. Definicion del directorio -------------------------------------------------
+if (grepl("danny", getwd())) {
+  wd <- "C:/Users/danny/OneDrive/Documentos/BD-ML/Taller1"
+} else {
+  wd <- getwd()
+}
+# 2. Script de Web-scraping ----------------------------------------------------
+
+# El script: "1. Scraping.R" realiza el proceso de web scraping para conseguir los datos
+setwd(paste0(wd,"/scripts"))
+source("1. Scraping.R")
+
+# El script: "2. Filtro Base y seleccion de variables.R" realiza  limpieza de la base de datos, mantiene las variables de interés y hace imputación de datos en missing values. 
+# Además, guarda la base de datos de interés (base_final.rds) que sera usada en los siguientes códigos.
+setwd(paste0(wd,"/scripts"))
+source("2. Filtrar Base.R")
+
+# El script: "3. Estadísticas descriptivas" realiza el análisis descriptivo de los datos. 
+setwd(paste0(wd,"/scripts"))
+source("3. Estadísticas Descriptivas.R")
+
+# El script: "4. Modelo ingreso-edad" realiza las estimaciones y análisis de datos de la sección 3 del Problem Set (Teoría edad-salario) 
+setwd(paste0(wd,"/scripts"))
+source("4. Modelo.R")
+
+# El script: "5. Evaluacion" realiza las estimaciones y análisis de datos de la sección 5 del Problem Set (Predicciones del ingreso) 
+setwd(paste0(wd,"/scripts"))
+source("5. Prediccion.R")
+
+# El script: "6. Modelo Gender_Earnings_Gap" realiza las estimaciones y análisis de datos de la sección 4 del Problem Set (Brecha salarial diferenciando por género) 
+setwd(paste0(wd,"/scripts"))
+source("6. M Gender Gap.R")
