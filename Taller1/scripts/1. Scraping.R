66382a982779ad9bf339fefd84b0e7b207161210
David-Duquino <d.duquino@uniandes.edu.co>
2026-02-21 02:01:40 -0500

    general_sctructure_scraping

---
+++ Taller1/scripts/1. Scraping.R
@@ -0,0 +1,8 @@
+
+#------------------------------------------------------------------------------#
+#----------------------------- Script 1. Scraping -----------------------------#
+#------------------------------------------------------------------------------#
+
+# El presente codigo carga la informacion de la Gran Encuesta Integrada de Hogares 
+# (GEIH) del repositorio del docente de la materia Ignacio Sarmiento 
+
