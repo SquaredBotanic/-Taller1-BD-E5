588cde4d09dcf695d0a13d573b04500b8208ae96
Bontiac <alejandra.davilar@konradlorenz.edu.co>
2026-02-21 01:39:16 -0500

    first_file_added

---
+++ Taller1/scripts/0. Script.R
@@ -0,0 +1,7 @@
+#------------------------------------------------------------------------------#
+#------------------------ CODIGO BASE - PROBLEM SET 1 -------------------------#
+#------------------------------------------------------------------------------#
+
+# El presente codigo permite:
+# 1: Cambiar el directorio entre cada uno de los colaboradores del proyecto
+# 2: Correr cada uno de los scripts utilizados en la resolucion del problem set 1.
